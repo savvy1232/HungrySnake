e1e654f088a1a89300f54d8a3307358f74247abe
Ranjit Sunil Somwanshi <ranjitdeshmukh1232@gmail.com>
2021-09-29 14:08:09 +0530

    Food Challenge

--- Hungry Snake/HungrySnake.py
+++ Hungry Snake/HungrySnake.py
@@ -4,32 +4,39 @@ from turtle import Turtle
 STARTING_POSITIONS = [(0,0), (-20,0), (-40,0)]
 
 MOVE_DISTANCE = 20
+
+
 UP = 90
 DOWN = 270
 LEFT = 180
 RIGHT = 0
 
-class Snake:
+class Snake(Turtle):
 
 
-    def __init__(self, new_segment=None) :
+    def __init__(self):
+        super().__init__()
         self.segments = []
         self.create_snake()
         self.head = self.segments[0]
-        new_segment.color('white')
-        new_segment.penup()                          # creating snake
-        new_segment.goto(position)
-        self.segments.append(new_segment)
 
-        def move(self):
-            def create_snake(self):
-                for position in STARTING_POSITIONS :
-                    new_segment = Turtle("square")
-                    for seg_num in range(len(self.segments) - 1, 0, -1):
-                        new_x = self.segments[seg_num - 1].xcor()
-                        new_y = self.segments[seg_num - 1].ycor()  # moving snake
-                        self.segments[seg_num].goto(new_x, new_y)
-                        self.head.forward(MOVE_DISTANCE)
+
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+    def move(self):
+
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()  # moving snake
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
 
     def up(self):
         if self.head.heading() != DOWN:
--- Hungry Snake/Main.py
+++ Hungry Snake/Main.py
@@ -1,5 +1,6 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from HungrySnake import Snake
+from Rat import Food
 import time
 
 screen = Screen()
@@ -10,6 +11,7 @@ screen.tracer(0)
 
 
 snake = Snake()
+Rat   = Food()
 
 screen.listen()
 screen.onkey(snake.up,"Up")
@@ -18,12 +20,19 @@ screen.onkey(snake.left,"Left")
 screen.onkey(snake.right,"Right")
 
 
+
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    if snake.head.distance(Rat)<15:
+      Rat.refresh()
+
+
+
+
 
 
 
